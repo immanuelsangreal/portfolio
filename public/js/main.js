e35fbfd100ec79311467d95f3858edeb8f3a62bf
immanuelsangreal <immanuelsangreal@gmail.com>
2025-07-04 01:37:37 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,151 +1,303 @@
-; (function ($) {
+/*
+Theme Name: Ovro - Personal Portfolio PHP Template
+Theme URI: #
+Author: Markey
+Author URI: #
+Version: 1.0.0
+*/
 
-    $(document).ready(function () {
-
-        //========== SIDEBAR/SEARCH AREA ============= //
-        $(".hamburger_menu").on("click", function (e) {
-            e.preventDefault();
-            $(".slide-bar").toggleClass("show");
-            $("body").addClass("on-side");
-            $('.body-overlay').addClass('active');
-            $(this).addClass('active');
-        });
-        $(".close-mobile-menu > a").on("click", function (e) {
-            e.preventDefault();
-            $(".slide-bar").removeClass("show");
-            $("body").removeClass("on-side");
-            $('.body-overlay').removeClass('active');
-            $('.hamburger_menu').removeClass('active');
-        });
-        //========== SIDEBAR/SEARCH AREA ============= //
-
-        //========== PAGE PROGRESS STARTS ============= // 
-        var progressPath = document.querySelector(".progress-wrap path");
-        var pathLength = progressPath.getTotalLength();
-        progressPath.style.transition = progressPath.style.WebkitTransition =
-            "none";
-        progressPath.style.strokeDasharray = pathLength + " " + pathLength;
-        progressPath.style.strokeDashoffset = pathLength;
-        progressPath.getBoundingClientRect();
-        progressPath.style.transition = progressPath.style.WebkitTransition =
-            "stroke-dashoffset 10ms linear";
-        var updateProgress = function () {
-            var scroll = $(window).scrollTop();
-            var height = $(document).height() - $(window).height();
-            var progress = pathLength - (scroll * pathLength) / height;
-            progressPath.style.strokeDashoffset = progress;
-        };
-        updateProgress();
-        $(window).scroll(updateProgress);
-        var offset = 50;
-        var duration = 550;
-        jQuery(window).on("scroll", function () {
-            if (jQuery(this).scrollTop() > offset) {
-                jQuery(".progress-wrap").addClass("active-progress");
-            } else {
-                jQuery(".progress-wrap").removeClass("active-progress");
-            }
-        });
-        jQuery(".progress-wrap").on("click", function (event) {
-            event.preventDefault();
-            jQuery("html, body").animate({ scrollTop: 0 }, duration);
-            return false;
-        });
-        //========== PAGE PROGRESS STARTS ============= // 
-
-        //========== VIDEO POPUP STARTS ============= //
-        if ($(".popup-youtube").length > 0) {
-            $(".popup-youtube").magnificPopup({
-                type: "iframe",
-            });
-        }
-        //========== VIDEO POPUP ENDS ============= //
-        AOS.init;
-        AOS.init({ disable: 'mobile' });
-
-        //========== NICE SELECT ============= //
-        $('select').niceSelect();
-
+(function ($) {
+  "use strict";
+  // Preloader
+  $(window).on("load", function () {
+    $("#preloader").fadeOut("slow", function () {
+      $(this).remove();
     });
-    //========== COUNTER UP============= //
-    const ucounter = $('.counter');
-    if (ucounter.length > 0) {
-        ucounter.countUp();
-    };
+  });
 
-    //========== PRELOADER ============= //
-    $(window).on("load", function (event) {
-        setTimeout(function () {
-            $("#preloader").fadeToggle();
-        }, 200);
+  /*=========================================
+  =            Intersection observer          =
+  =========================================*/
+  if (!!window.IntersectionObserver) {
+    let observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("active-animation");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        rootMargin: "0px 0px -100px 0px",
+      }
+    );
+    document.querySelectorAll(".has-animation").forEach((block) => {
+      observer.observe(block);
+    });
+  } else {
+    document.querySelectorAll(".has-animation").forEach((block) => {
+      block.classList.remove("has-animation");
+    });
+  }
 
+  /*=========================================
+  =            mobile menu          =
+  =========================================*/
+  // mobile menu
+  $("#hamburger").on("click", function () {
+    $(".mobile-nav").addClass("show");
+    $(".overlay").addClass("active");
+  });
+
+  $(".close-nav").on("click", function () {
+    $(".mobile-nav").removeClass("show");
+    $(".overlay").removeClass("active");
+  });
+
+  $(".overlay").on("click", function () {
+    $(".mobile-nav").removeClass("show");
+    $(".overlay").removeClass("active");
+  });
+
+  $("#mobile-menu").metisMenu();
+
+  /*=========================================
+  =             magnific-popup          =
+  =========================================*/
+  $(".video-play-btn").magnificPopup({
+    type: "video",
+  });
+  $(".img-popup").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+
+  /*=========================================
+  =             Aos          =
+  =========================================*/
+  AOS.init({
+    once: true,
+    duration: 1000,
+  });
+
+  //  Counter Js
+  if ($(".counter").length) {
+    $(".counter").counterUp({
+      delay: 10,
+      time: 1000,
+    });
+  }
+
+  /*=========================================
+  =            Theme Setting          =
+  =========================================*/
+
+  $(".icon-bar-2").on("click", function () {
+    $(".theme-setting-area").toggleClass("show");
+  });
+  $(".theme-setting-area .close").on("click", function () {
+    $(".theme-setting-area").removeClass("show");
+  });
+
+  /*=========================================
+  =            Wow Js          =
+  =========================================*/
+  new WOW().init();
+
+  /*=========================================
+  =             slick-slider          =
+  =========================================*/
+  $(".testimonial-slider-active").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows: false,
+  });
+
+  $(".marquee-slider-active").slick({
+    dots: false,
+    infinite: true,
+    speed: 10000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    variableWidth: true,
+    pauseOnHover: false,
+  });
+
+  $(".marquee-slider-active-2").slick({
+    dots: false,
+    infinite: true,
+    speed: 10000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    variableWidth: true,
+    rtl: true,
+    pauseOnHover: false,
+  });
+
+  $(".portfolio-slider-active").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    adaptiveHeight: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  /*=========================================
+  =            GSAP          =
+  =========================================*/
+  const splitText = new SplitText(".h2-2", { type: "words, chars" });
+  const chars = splitText.chars;
+
+  gsap.from(chars, {
+    duration: 1,
+    opacity: 0,
+    y: 10,
+    ease: "power2.inOut",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".h2-2",
+      start: "top 80%", // Adjust start position as needed
+      end: "top 20%", // Adjust end position as needed
+      // markers: true, // Uncomment for debugging
+    },
+  });
+
+  /*=========================================
+  =            Cursor          =
+  =========================================*/
+  $(".procus-cursor").on("mouseenter", function () {
+    $(".mouse-cursor").addClass("cursor-big");
+  });
+  $(".procus-cursor").on("mouseleave", function () {
+    $(".mouse-cursor").removeClass("cursor-big");
+  });
+
+  var a = document.querySelector(".procus-cursor");
+  var b = document.querySelector(".procus-cursor2");
+
+  if (a, b) {
+    document.addEventListener("mousemove", (e) => {
+      a.setAttribute("style", "transform: translate3d(calc(-50% + " + e.clientX + "px), calc(-50% + " + e.clientY + "px), 0px) ");
     });
 
-    //========== POPUP AREA ============= //
-    $(".click-here").on('click', function () {
-        $(".custom-model-main").addClass('model-open');
+    document.addEventListener("mousemove", (e) => {
+      b.setAttribute("style", "top: " + e.clientY + "px; left: " + e.clientX + "px;");
     });
-    $(".close-btn, .bg-overlay").click(function () {
-        $(".custom-model-main").removeClass('model-open');
-    });
+  }
 
+  /*=========================================
+  =            Smooth Scroll          =
+  =========================================*/
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true,
+  });
+
+  /*=========================================
+  =            popup          =
+  =========================================*/
+  $(".click-here").on("click", function () {
+    $(".custom-model-main").addClass("model-open");
+  });
+
+  $(".close-btn, .bg-overlay").click(function () {
+    $(".custom-model-main").removeClass("model-open");
+  });
+  $(".popup-close-btn").click(function () {
+    $(".portfolio-popup").removeClass("model-open");
+  });
+
+  // Nice Select
+  $(".nice-select").niceSelect();
+  $(".nice-select6").niceSelect();
+
+  /*=========================================
+  =            sticky header          =
+  =========================================*/
+
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 250) {
+      $(".header-area").addClass("sticky");
+    } else {
+      $(".header-area").removeClass("sticky");
+    }
+  });
+
+  /*=========================================
+  =            scroll-top          =
+  =========================================*/
+  //   scroll-top
+  var progressPath = document.querySelector(".progress-wrap path");
+  var pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
+  var updateProgress = function () {
+    var scroll = $(window).scrollTop();
+    var height = $(document).height() - $(window).height();
+    var progress = pathLength - (scroll * pathLength) / height;
+    progressPath.style.strokeDashoffset = progress;
+  };
+  updateProgress();
+  $(window).scroll(updateProgress);
+  var offset = 50;
+  var duration = 550;
+  jQuery(window).on("scroll", function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery(".progress-wrap").addClass("active-progress");
+    } else {
+      jQuery(".progress-wrap").removeClass("active-progress");
+    }
+  });
+  jQuery(".progress-wrap").on("click", function (event) {
+    event.preventDefault();
+    jQuery("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      duration
+    );
+    return false;
+  });
+
+  /*=========================================
+  =            Active          =
+  =========================================*/
+  $(".list-group-item").on("click", function () {
+    $(this).siblings(".active").removeClass("active");
+    $(this).addClass("active");
+  });
 })(jQuery);
 
-//========== GSAP AREA ============= //
-if ($('.reveal').length) { gsap.registerPlugin(ScrollTrigger); let revealContainers = document.querySelectorAll(".reveal"); revealContainers.forEach((container) => { let image = container.querySelector("img"); let tl = gsap.timeline({ scrollTrigger: { trigger: container, toggleActions: "play none none none" } }); tl.set(container, { autoAlpha: 1 }); tl.from(container, 1.5, { xPercent: -100, ease: Power2.out }); tl.from(image, 1.5, { xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out }); }); }
 
-// Theme toggle functionality
-const toggleButton = document.getElementById('theme-toggle');
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('light-mode');
-    toggleButton.checked = true;
-}
-toggleButton.addEventListener('change', () => {
-    document.body.classList.toggle('light-mode');
-
-    if (document.body.classList.contains('light-mode')) {
-        localStorage.setItem('theme', 'light');
-    } else {
-        localStorage.setItem('theme', 'dark-mode');
-    }
-});
-
-// UPDATE: I was able to get this working again... Enjoy!
-var cursor = document.querySelector('.procus-cursor');
-var cursorinner = document.querySelector('.procus-cursor2');
-var a = document.querySelectorAll('a');
-
-document.addEventListener('mousemove', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-});
-
-document.addEventListener('mousemove', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursorinner.style.left = x + 'px';
-    cursorinner.style.top = y + 'px';
-});
-
-document.addEventListener('mousedown', function () {
-    cursor.classList.add('click');
-    cursorinner.classList.add('cursorinnerhover')
-});
-
-document.addEventListener('mouseup', function () {
-    cursor.classList.remove('click')
-    cursorinner.classList.remove('cursorinnerhover')
-});
-
-a.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        cursor.classList.add('hover');
-    });
-    item.addEventListener('mouseleave', () => {
-        cursor.classList.remove('hover');
-    });
-})
 // =======================================================================================
 //  My Custom Code for Portfolio Video Popup
 // =======================================================================================
@@ -159,7 +311,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const popupTitle = popup.querySelector('.popup-title');
     const popupIframe = popup.querySelector('iframe');
     const closeBtn = popup.querySelector('.popup-close-btn');
-    const popupContainer = popup.querySelector('.popup-container');
 
     const openPopup = (title, videoUrl) => {
         if (!popupTitle || !popupIframe) return;
@@ -190,12 +341,11 @@ document.addEventListener("DOMContentLoaded", function() {
     if (closeBtn) {
         closeBtn.addEventListener('click', closePopup);
     }
-    if (popup) {
-         popup.addEventListener('click', function(e) {
-            // Close if clicking on the background overlay itself, not its children
-            if (e.target === popup) {
-                closePopup();
-            }
-        });
-    }
+    popup.addEventListener('click', function(e) {
+        // Close if clicking on the background overlay itself, not its children
+        if (e.target === popup) {
+            closePopup();
+        }
+    });
 });
+
